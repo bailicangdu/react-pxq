81d8fda9cb6a96b63ec6a2bfeccd285b05d58bd5
maguohua <1264889788@qq.com>
2016-11-18 11:07:43 +0800

    updata

--- server.js
+++ server.js
@@ -5,8 +5,8 @@ var config = require('./webpack.config.dev');
 //代理服务器
 var proxy = [{
 	path: '/*/*', //必须得有一个文件地址，如果顶层文件夹名字不同，则用/*代替
-	target: 'http://shopro.putaoevent.com',
-	host: 'shopro.putaoevent.com',
+	target: 'http://ptstarring.ptdev.cn',
+	host: 'ptstarring.ptdev.cn',
 	secure: false
 }];
 var server = new WebpackDevServer(webpack(config), {
