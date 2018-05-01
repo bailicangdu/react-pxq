045008ad5c31f58bbef6a0382197bf70f1b7c3eb
cangdu <1264889788@qq.com>
2018-05-01 19:13:24 +0800

    remove http

--- src/envconfig/envconfig.js
+++ src/envconfig/envconfig.js
@@ -2,11 +2,11 @@
  * 全局配置文件
  */
 let baseURL; 
-let imgUrl = 'http://elm.cangdu.org/img/';
+let imgUrl = '//elm.cangdu.org/img/';
 if(process.env.NODE_ENV === 'development'){
-  baseURL = 'http://api.cangdu.org';
+  baseURL = '//api.cangdu.org';
 }else{
-  baseURL = 'http://api.cangdu.org';
+  baseURL = '//api.cangdu.org';
 }
 
 
