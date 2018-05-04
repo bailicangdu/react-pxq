e6f534fd04d068288f3175b73338116dccfe59de
cangdu <1264889788@qq.com>
2018-05-04 17:17:15 +0800

    init ssr

--- src/store/production/action-type.js
+++
@@ -1,8 +0,0 @@
-// 保存商品数据
-export const GETPRODUCTION = 'GETPRODUCTION';
-// 选择商品
-export const TOGGLESELECT = 'TOGGLESELECT';
-// 编辑商品
-export const EDITPRODUCTION = 'EDITPRODUCTION';
-// 清空选择
-export const CLEARSELECTED = 'CLEARSELECTED';
